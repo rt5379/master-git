3981733e351d082e34c1dd4ec71f6393cdee66cf
rt5379 <ravitejasangeethamnew@gmail.com>
2025-01-12 18:54:24 +0530

    sample commit

---
+++ sample.java
@@ -0,0 +1,8 @@
+public class sample{
+    public static void main(String args[]){
+        int a = 10;
+        int b = 20;
+        int sum = a+ b;
+        System.out.println("Hello world:"+ sum);
+    }
+}
